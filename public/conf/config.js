033e635b882b4d49564e6f5c7590cd5e19466270
Carlos <carmarper5@hotmail.com>
2018-06-04 22:55:22 +0200

    añadido confi.js

--- public/conf/config.js
+++ public/conf/config.js
@@ -1,5 +1,14 @@
-{
-	"protocol": "https://",
-	"domain": "carlosmp.com/",
-	"port": 4001
-}
+
+const CERTIFICATE_PATH = {
+    caBundle: "../../carlosmp_com.ca-bundle",
+    clavePrivada: "../../clave_carlosmp_com.key",
+    certificado: "../../carlosmp_com.crt"
+};
+
+const URL_CONFIG = {
+    protocol: "https",
+    domain: "carlosmp.com",
+    port: 4001
+};
+
+const HOME_URL = URL_CONFIG.protocol + "://" + URL_CONFIG.domain + (URL_CONFIG.port ? ":" + URL_CONFIG.port : "");
